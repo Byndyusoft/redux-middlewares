0917e7b4fff2c6bd748061e7e59e3922b2c8671d
VadimZvf <vadim.zvf@gmail.com>
2017-10-12 15:27:43 +0500

    Добавлено самостоятельное отлавливание ошибок

--- src/error-logger/index.js
+++ src/error-logger/index.js
@@ -1,10 +1,9 @@
 /**
  * @param {number} stackLength - length of state stack to log
- * @param {string} typeOfErrorAction - substr or name of action
  * @param {function} onError - on error handler
  * @param {function} prepareState
  */
-export default (stackLength, typeOfErrorAction, onError, prepareState = state => state) => {
+export default (stackLength, onError, prepareState = state => state) => {
     const states = new Array(stackLength);
 
     return store => next => action => {
@@ -12,9 +11,11 @@ export default (stackLength, typeOfErrorAction, onError, prepareState = state =>
         state.action = action.type;
         states.unshift(Object.assign({}, state));
         states.pop();
-        if (action.type.indexOf(typeOfErrorAction) !== -1) {
-            onError(states, action.payload.message, action.payload.stack);
+        try {
+            next(action);
+        } catch (e) {
+            onError(states, e, action);
+            throw e;
         }
-        next(action);
     };
 };
